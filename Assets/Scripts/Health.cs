2a9b461f037318a8041bd25891ddc318e96b61b9
Jonathan Dervin <jdervin@ltu.edu>
2021-02-01 12:32:07 -0500

    HealthBar
    Health Script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int MaxHP;
+    [HideInInspector]
+    public int currentHP;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Damage")
+        {
+            currentHP = currentHP - 1;
+            Destroy(other.gameObject);
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = MaxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
